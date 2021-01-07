c6e2635b46e091503293c4034adf717b9c49b1cd
vivintsmartvideo <chris.hall@vivint.com>
2021-01-07 10:39:27 -0700

    png as default output and size in fraction of the image

--- PythonAPI/pycocotool/cocoeval.py
+++ PythonAPI/pycocotool/cocoeval.py
@@ -581,7 +581,7 @@ class COCOeval:
 
             # if save_dir is set, figure will be saved to save_dir
             if save_dir is not None:
-                save_file = '%s/%s.pdf' % (save_dir, plotname)
+                save_file = '%s/%s.png' % (save_dir, plotname)
                 print('save figure to %s' % save_file)
                 fig.savefig(save_file)
             fig.canvas.draw()
@@ -702,7 +702,9 @@ class Params:
         self.iouThrs = np.linspace(.5, 0.95, int(np.round((0.95 - .5) / .05)) + 1, endpoint=True)
         self.recThrs = np.linspace(.0, 1.00, int(np.round((1.00 - .0) / .01)) + 1, endpoint=True)
         self.maxDets = [1, 10, 100]
-        self.areaRng = [[0 ** 2, 1e5 ** 2], [0 ** 2, 32 ** 2], [32 ** 2, 96 ** 2], [96 ** 2, 1e5 ** 2]]
+        #self.areaRng = [[0 ** 2, 1e5 ** 2], [0 ** 2, 32 ** 2], [32 ** 2, 96 ** 2], [96 ** 2, 1e5 ** 2]]
+        # area in image fraction - converting based on image size of 480x640
+        self.areaRng = [[0.0 ** 2, 1.0 ** 2], [0.0 ** 2, 0.0577 ** 2], [0.0577 ** 2, 0.173 ** 2], [0.173 ** 2, 1.0 ** 2]]
         self.areaRngLbl = ['all', 'small', 'medium', 'large']
         self.useCats = 1
 
@@ -721,7 +723,9 @@ class Params:
         self.iouThrs = np.linspace(.5, 0.95, int(np.round((0.95 - .5) / .05)) + 1, endpoint=True)
         self.recThrs = np.linspace(.0, 1.00, int(np.round((1.00 - .0) / .01)) + 1, endpoint=True)
         self.maxDets = [20]
-        self.areaRng = [[0 ** 2, 1e5 ** 2], [32 ** 2, 96 ** 2], [96 ** 2, 1e5 ** 2]]
+        #self.areaRng = [[0 ** 2, 1e5 ** 2], [32 ** 2, 96 ** 2], [96 ** 2, 1e5 ** 2]]
+        # area in image fraction - converting based on image size of 480x640
+        self.areaRng = [[0.0 ** 2, 1.0 ** 2], [0.0577 ** 2, 0.173 ** 2], [0.173 ** 2, 1.0 ** 2]]
         self.areaRngLbl = ['all', 'medium', 'large']
         self.useCats = 1
         self.kpt_oks_sigmas = np.array([.26, .25, .25, .35, .35, .79, .79, .72, .72, .62,.62, 1.07, 1.07, .87, .87, .89, .89])/10.0

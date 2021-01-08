eed45744d07cc4ecc14643a5a2537ed40d2f4dce
vivintsmartvideo <chris.hall@vivint.com>
2021-01-08 16:18:23 -0700

    removed debug print statements

--- PythonAPI/pycocotools/cocoeval.py
+++ PythonAPI/pycocotools/cocoeval.py
@@ -582,15 +582,9 @@ class COCOeval:
             # if save_dir is set, figure will be saved to save_dir
             if save_dir is not None:
                 save_file = '%s/%s.png' % (save_dir, plotname)
-                print('save figure to %s' % save_file)
                 fig.savefig(save_file)
                 import pandas as pd
-                print(f'ds.shape {ds.shape}')
-                print(f'recThrs.shape {recThrs.shape}')
-                print(f'ps.shape {ps.shape}')
-                print(f'ap.shape {ap.shape}')
                 ls.append('recThrs')
-                #df = pd.DataFrame(np.concatenate((ps.T, np.expand_dims(recThrs, axis=0))), columns=ls)
                 df = pd.DataFrame(np.concatenate((ps, np.expand_dims(recThrs, axis=0))).T, columns=ls)
                 df.to_csv(save_file[:-4]+'.csv')
 

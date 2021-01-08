f45583e9f627d5da2ddee9cd6085ed3f34fd0cc9
vivintsmartvideo <chris.hall@vivint.com>
2021-01-08 13:42:35 -0700

    prepping for pip installation

--- PythonAPI/setup.py
+++ PythonAPI/setup.py
@@ -20,8 +20,9 @@ setup(
     install_requires=[
         'setuptools>=18.0',
         'cython>=0.27.3',
-        'matplotlib>=2.1.0'
+        'matplotlib>=2.1.0',
+        'numpy'
     ],
-    version='2.0',
+    version='3.0',
     ext_modules= ext_modules
 )
